68304317f47183a1d48fda6f802a42b19d90fd91
Piotr Kawczyński <kawczyniak@gmail.com>
2020-01-14 23:35:44 +0100

    Clean up code

--- src/routers/auth.ts
+++ src/routers/auth.ts
@@ -4,7 +4,6 @@ import { auth } from "../controllers"
 export const router = express.Router()
 
 router.post("/register", auth.register)
-
 router.post("/login", auth.login)
 router.post("/remain-password", auth.remainPasswordSendMail)
 router.post("/change-password", auth.changePassword)
